1b8f51bb81ccae1b71fe3f8af77bd5a135f262e4
Olim Tursunov <olimtoy83@gmail.com>
2026-08-06 13:59:31 +0500

    Refactor modal and form logic using classes. 2

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import './homework-8.js';
 import './homework-9.js';
 import './homework-10.js';
 import './homework-11.js';
-import './homework-12.js';
+// import './homework-12.js';
 
 class Car {
   constructor(brand, model, maxSpeed, color) {
