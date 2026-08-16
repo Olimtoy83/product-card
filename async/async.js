6767cb096c0e2b400f8cf33e80481ffec4e75c85
Olim Tursunov <olimtoy83@gmail.com>
2026-08-16 17:57:21 +0500

    (Homework №15):Applied asynchronicity in JS.

---
+++ async/async.js
@@ -0,0 +1,131 @@
+function loadUsers() {
+  const usersJSON = localStorage.getItem("users");
+
+  if (usersJSON === null) {
+    fetch("users.json")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Ошибка при загрузке данных");
+        }
+
+        return response.json();
+      })
+      .then((data) => {
+        setTimeout(() => {
+          renderUsers(data.users);
+        }, 3000);
+
+        localStorage.setItem("users", JSON.stringify(data));
+      })
+      .catch((error) => {
+        console.error(error);
+
+        const container = document.getElementById("app");
+        container.innerHTML = "Ошибка при загрузке данных";
+      });
+  } else {
+    const users = JSON.parse(usersJSON);
+    renderUsers(users.users);
+  }
+}
+
+loadUsers();
+
+function renderUsers(users) {
+  const container = document.getElementById("app");
+  const userCardCopy = document.getElementById("users-template");
+
+  container.innerHTML = "";
+
+  users.forEach((user) => {
+    const cardClone = userCardCopy.content.cloneNode(true);
+
+    cardClone.querySelector(".user-name").textContent = user.name;
+    cardClone.querySelector(".user-surname").textContent = user.surname;
+    cardClone.querySelector(".user-email").textContent = user.email;
+    cardClone.querySelector(".user-age").textContent = user.age;
+    cardClone.querySelector(".user-city").textContent = user.city;
+
+    const deleteBtn = cardClone.querySelector(".delete-card-btn");
+    const cardElement = cardClone.querySelector(".user-card");
+
+    deleteBtn.addEventListener("click", () => {
+      const usersJSON = localStorage.getItem("users");
+      const usersData = JSON.parse(usersJSON);
+
+      usersData.users = usersData.users.filter((item) => {
+        return item.id !== user.id;
+      });
+
+      localStorage.setItem("users", JSON.stringify(usersData));
+
+      cardElement.remove();
+    });
+
+    container.appendChild(cardClone);
+  });
+}
+
+const deleteAllBtn = document.getElementById("delete-all-btn");
+
+deleteAllBtn.addEventListener("click", () => {
+  const usersJSON = localStorage.getItem("users");
+
+  if (!usersJSON) {
+    alert("Карточек нет");
+    return;
+  }
+
+  const usersData = JSON.parse(usersJSON);
+
+  if (usersData.users.length === 0) {
+    alert("Все карточки уже удалены");
+    return;
+  }
+
+  usersData.users = [];
+
+  localStorage.setItem("users", JSON.stringify(usersData));
+
+  const container = document.getElementById("app");
+  container.innerHTML = "";
+});
+
+const getAllBtn = document.getElementById("get-all-btn");
+
+getAllBtn.addEventListener("click", () => {
+  const existingCards = document.querySelectorAll(".user-card");
+
+  fetchUsersData()
+    .then((data) => {
+      if (!data) {
+        return;
+      }
+
+      if (data.users.length === existingCards.length) {
+        alert("Все карточки уже отображены");
+      } else {
+        renderUsers(data.users);
+        localStorage.setItem("users", JSON.stringify(data));
+      }
+    });
+});
+
+function fetchUsersData() {
+  return fetch("users.json")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Ошибка данных");
+      }
+
+      return response.json();
+    })
+    .catch((error) => {
+      console.error(error);
+
+      const container = document.getElementById("app");
+      container.innerHTML = "Ошибка при загрузке данных";
+
+      return null;
+    });
+}
